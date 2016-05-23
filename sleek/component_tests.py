eabe5870a882515708431dc0e3d9195e6e2b23ca
abunuwas <joseharoperalta@gmail.com>
2016-05-23 17:42:39 +0100

    Add package for streaming/queueing service

--- sleek/component_tests.py
+++ sleek/component_tests.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-    SleekXMPP: The Sleek XMPP Library
-    Copyright (C) 2010  Nathanael C. Fritz
-    This file is part of SleekXMPP.
-
-    See the file LICENSE for copying permission.
-"""
+from queueing import stream
 
 import sys
 import logging
