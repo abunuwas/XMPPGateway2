35d5ac15eee9a3e5769f991be3b279a37f720827
abunuwas <joseharoperalta@gmail.com>
2016-05-24 15:27:46 +0100

    Start writing setup.py file for package installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='XMPPGateway',
+		version='1.0',
+		description='Scalable Python implementation of an XMPP Component connection using SleekXMPP library.',
+		author='Jose Haro, Robert butler',
+		author_email='joseh@intamac.com, robertb@intamac.com',
+		url='',
+		license='',
+		py_modules=['sleek'],
+		)
