7a131de8762b758e0d92326ff4fab2e9d3d39b59
abunuwas <joseharoperalta@gmail.com>
2016-05-24 16:32:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='XMPPGateway',
 		author_email='joseh@intamac.com, robertb@intamac.com',
 		url='',
 		license='',
-		py_modules=['sleek', 'custom_stanzas', 'queueing', 'stream'],
+		packages=['sleek', 'queueing'],
 		)
