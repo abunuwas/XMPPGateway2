f2eee649dd10429498bdd6662ff926a9cdc4b8a9
abunuwas <joseharoperalta@gmail.com>
2016-06-03 16:44:33 +0100

    Add experiments with SQS

--- queueing_system/sqs_q/consumer.py
+++ queueing_system/sqs_q/consumer.py
@@ -5,7 +5,9 @@ from botocore.exceptions import ClientError
 
 sqs = boto3.resource('sqs')
 
-queue_name = 'test-queue'
+queue_name = 'test'
+
+outcomes = {}
 
 def get_queue(queue_name):
 	try:
@@ -14,10 +16,36 @@ def get_queue(queue_name):
 	except ClientError:
 		print('The requested queue does not exist.')
 
-def poll(queue):
-	for messages in queue.receive_messages():
-		return message
+def poll(queue, p_id):
+	while True:
+		for message in queue.receive_messages():
+			device, n = message.body.split('_')
+			print(device, n, p_id)
+			#if device not in outcomes.keys():
+			#	outcomes[device] = [message.body]
+			#else:
+			#	outcomes[device].append(message.body)
+			message.delete()
 
 if __name__ == '__main__':
+	import multiprocessing as mp
 	queue = get_queue(queue_name)
-	message = poll(queue)
+	#message = poll(queue)
+	jobs = []
+	for i in range(5):
+		process = mp.Process(target=poll, args=(queue, i))
+		jobs.append(process)
+	for j in jobs:
+		j.start()
+	'''
+	try:
+		poll(queue)
+	except KeyboardInterrupt:
+		for key, data in outcomes.items():
+			print('\n\n')
+			print(key)
+			print('-'*10)
+			for value in data:
+				print(value)
+	'''
+
--- queueing_system/sqs_q/producer.py
+++ queueing_system/sqs_q/producer.py
@@ -1,11 +1,12 @@
 import datetime
+import time
 
 import boto3
 from botocore.exceptions import ClientError
 
 sqs = boto3.resource('sqs')
 
-queue_name = 'test-queue'
+queue_name = 'test'
 
 def get_queue(queue_name):
 	try:
@@ -23,6 +24,31 @@ def push(queue, payload, _from):
 						)
 	return response
 
+def test(queue, device):
+	e = 0
+	while e < 30:
+		response = queue.send_message(MessageBody=device+'_'+str(e),
+							MessageAttributes={'TimeStamp': {
+															'StringValue': str(datetime.datetime.now()),
+															'DataType': 'String'
+															}
+							})
+		print(response)
+		time.sleep(.3)
+		e += 1
+
 if __name__ == '__main__':
+	import threading
 	queue = get_queue(queue_name)
-	response = push(queue, 'This is a message', 'Camera_1')
+	#test(queue)
+	#response = push(queue, 'This is a message', 'Camera_1')
+	threads = []
+	devices = ['camera', 'DVR', 'motionDetector', 'car', 'computer', 'fireAlarm']
+	for i in range(1):
+		thread = threading.Thread(target=test, args=(queue, devices[i]))
+		threads.append(thread)
+	e=0
+	for thread in threads:
+		thread.start()
+		print('started thread n. ', e)
+		e+=1
